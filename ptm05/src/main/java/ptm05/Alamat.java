82ddb08dcd9c920e58cd4f4ee8468c19f3c41e8d
muhamdan <danhamx2@gmail.com>
2023-05-10 14:38:56 +0700

    Initial commit

---
+++ ptm05/src/main/java/ptm05/Alamat.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ptm05;
+public class Alamat {
+    
+    String kota;
+    String kecamatan;
+    String desa;
+    int no; 
+    
+    void info(){
+        System.out.println("Alamat Rumah kota "+kota+" Kec, "+kecamatan +" Kel, " +desa+ " No," +no);
+    
+}
+}
---
+++ ptm05/src/main/java/ptm05/Ptm05.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ptm05;
+
+public class Ptm05 {
+
+    public static void main(String[] args) {
+    
+    
+    Alamat alamat = new Alamat();
+    alamat.kota = "Banjarbaru";
+    alamat.kecamatan = "Cempaka";
+    alamat.desa = "Bangkal";
+    alamat.no = 48;
+    
+    Alamat alamatmu = new Alamat();
+    alamatmu.kota = "Banjarmasin";
+    alamatmu.kecamatan = "Aluh Aluh";
+    alamatmu.desa = "Bakambat";
+    alamatmu.no = 21;
+    
+    Alamat alamatku = new Alamat();
+    alamatku.kota = "Banjarbaru";
+    alamatku.kecamatan = "Landasan Ulin";
+    alamatku.desa = "G. Manggis";
+    alamatku.no = 11;
+    
+    alamat.info();
+    alamatmu.info();
+    alamatku.info();
+    
+    }
+}
